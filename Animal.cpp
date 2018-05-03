fc9b4a8ff2d8a68d91264e9f2df74a24471a7cb3
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-05-02 20:05:35 -0500

    commit

--- Animal.cpp
+++ Animal.cpp
@@ -33,5 +33,8 @@ void Animal::set_species(string spec){
 string Animal::get_species(){
 	return species;
 }
+string Animal::describe(){
+	return species << "with" << number_of_legs << "legs";
+}
 
 
--- CIS2013_Week15_lab1.cpp
+++ CIS2013_Week15_lab1.cpp
@@ -10,6 +10,7 @@ int main(){
 	int legs;
 	char alive;
 	bool is_alive;
+	Animal cat;
 
 	cout<< "You making some animals..." << endl;
 	cout<< "What species of animal do you to make first? ";
@@ -28,3 +29,6 @@ int main(){
 	}else{
 		cout<<"That was not an option...screw you....I'm killing your animal" << endl;
 	}
+
+	Animal users(legs, is_alive, species);
+	cout<< "Also I am going to make you a cat"<<endl;
